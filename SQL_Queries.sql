53e2721bd4b4045d1f50207ceafa251e8067e148
ArjunNG <arjunng@gmail.com>
2019-12-21 17:51:21 -0600

    more changes

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -1,72 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/GAZ6wz
-
-CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "dept_employee" (
-    "emp_no" integer   NOT NULL,
-    "dept_no" varchar   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" integer   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-CREATE TABLE "employees" (
-    "emp_no" integer   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "gender" varchar(1)   NOT NULL,
-    "hiredate" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" integer   NOT NULL,
-    "salary" integer   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "emp_no" integer   NOT NULL,
-    "title" varchar   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
 -- Queries to see all the records of the table after importing data from CSV files:
 
 select max(emp_no) from employees;
@@ -98,7 +29,7 @@ gender "Gender",
 hiredate "Hire Date" 
 from employees where hiredate between '1986-01-01' and '1986-12-31' order by hiredate;
 
---3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates:
+3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates:
 
 select d.dept_no "Department Number",
 d.dept_name "Department Name",
