c28045e9c2355be3f6ec72ff1238b742a32638a6
ArjunNG <arjunng@gmail.com>
2019-12-21 22:17:10 -0600

    More updates

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -1,6 +1,6 @@
 -- Queries to see all the records of the table after importing data from CSV files:
 
-select max(emp_no) from employees;
+select * from employees;
 select * from dept_employee;
 select * from dept_manager;
 select * from departments;
@@ -9,9 +9,9 @@ select * from titles;
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Data Analysis:
+-- Data Analysis:
 
-1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 
 select e.emp_no "Employee Number", e.last_name "Last Name", e.first_name "First Name",e.gender "Gender",
 s.salary "Salary"
@@ -19,7 +19,7 @@ from employees e
 inner join salaries s
 on e.emp_no=s.emp_no order by e.emp_no;
 
-2. List employees who were hired in 1986:
+-- 2. List employees who were hired in 1986:
 
 select emp_no "Employee Number",
 birth_date "Birth Date",
@@ -29,7 +29,7 @@ gender "Gender",
 hiredate "Hire Date" 
 from employees where hiredate between '1986-01-01' and '1986-12-31' order by hiredate;
 
-3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates:
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates:
 
 select d.dept_no "Department Number",
 d.dept_name "Department Name",
@@ -46,7 +46,7 @@ inner join
 employees e
 on dm.emp_no = e.emp_no order by e.emp_no;
 
-4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 select e.emp_no "Employee Number",
 e.last_name "Last Name",
@@ -59,8 +59,7 @@ inner join
 departments d
 on de.dept_no=d.dept_no order by e.emp_no;
 
-
-5. List all employees whose first name is "Hercules" and last names begin with "B."
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 
 select emp_no "Employee Number",
 birth_date "Birth Date",
@@ -70,7 +69,7 @@ gender "Gender",
 hiredate "Hire Date" 
 from employees where first_name = 'Hercules' and last_name like 'B%';
 
-6. List all employees in the Sales department, including their employee number, 
+-- 6. List all employees in the Sales department, including their employee number, 
 last name, first name, and department name:
 
 select e.emp_no "Employee Number",
@@ -85,7 +84,7 @@ departments d
 on de.dept_no=d.dept_no 
 where d.dept_name='Sales' order by e.emp_no;
 
-7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 select e.emp_no "Employee Number",
 e.last_name "Last Name",
@@ -99,14 +98,10 @@ departments d
 on de.dept_no=d.dept_no 
 where d.dept_name in ('Sales','Development') order by e.emp_no;
 
-8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
 select last_name "Last Name",
 count(last_name) "Frequency of Emp. with same Last Names" 
 from employees 
 group by last_name 
 order by count(last_name) desc;
-
-
-
-
